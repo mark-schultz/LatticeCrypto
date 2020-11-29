e62f528986b0f046c4a19d895416523e41627436
Mark Schultz <mdschultz@eng.ucsd.edu>
2020-11-29 10:55:15 -0800

    Begin implementing alga traits from scratch :(

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 #![feature(min_const_generics)]
 
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use std::convert::From;
 
-trait Ring: Add + Sub + Mul + Sized {}
+trait AdditiveAbelianGroup: Add + AddAssign + Sub + SubAssign + Neg + Sized {}
+trait AbstractRing: AdditiveAbelianGroup + Mul + MulAssign {}
+// Ring is modelled as "finite rank" of rank RANK, i.e. as a free module of rank RANK over (some
+// algebraic subset of) `u32`
+trait Ring<const N: usize>: AbstractRing + From<[u32; N]> {}
 
-impl<R: Add + Sub + Mul + Sized> Ring for R {}
-
-/// An element of Mat_{N x M}(R)
-struct Matrix<R : Ring, const N : usize, const M : usize>([[R;M];N]);
+impl<R: AbstractRing + From<[u32; N]>, const RANK : usize> Ring<RANK> for R {}
 
 mod modular_arithmetic {
     use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
@@ -18,9 +20,9 @@ mod modular_arithmetic {
     // as const generic parameter Q can't depend on other
     // generic types (such as T where T : Unsigned).
 
-    impl<const Q: u32> Modular<Q> {
-        fn new(x: u32) -> Self {
-            Modular(x % Q)
+    impl<const Q: u32> From<[u32; 1]> for Modular<Q> {
+        fn from(x: [u32;1]) -> Self {
+            Modular(x[0] % Q)
         }
     }
 
@@ -31,16 +33,9 @@ mod modular_arithmetic {
         where
             Self: Sized,
         {
-            Modular::new(self.0 + other.0)
+            Modular::from([self.0 + other.0])
         }
     }
-
-    impl<const Q: u32> AddAssign<Modular<Q>> for Modular<Q> {
-        fn add_assign(&mut self, other: Self) {
-            *self = self.add(other)
-        }
-    }
-
     impl<const Q: u32> Mul<Modular<Q>> for Modular<Q> {
         type Output = Modular<Q>;
 
@@ -53,15 +48,9 @@ mod modular_arithmetic {
             let x: u64 = self.0.into();
             let y: u64 = other.0.into();
             let modulus: u64 = Q.into();
-            Modular::new((x * y % modulus) as u32)
+            Modular::from([(x * y % modulus) as u32])
         }
     }
-    impl<const Q: u32> MulAssign<Modular<Q>> for Modular<Q> {
-        fn mul_assign(&mut self, other: Self) {
-            *self = self.mul(other)
-        }
-    }
-
     impl<const Q: u32> Sub<Modular<Q>> for Modular<Q> {
         type Output = Modular<Q>;
 
@@ -69,15 +58,9 @@ mod modular_arithmetic {
         where
             Self: Sized,
         {
-            Modular::new(self.0 + other.neg().0)
+            Modular::from([self.0 + other.neg().0])
         }
     }
-    impl<const Q: u32> SubAssign<Modular<Q>> for Modular<Q> {
-        fn sub_assign(&mut self, other: Self) {
-            *self = self.sub(other)
-        }
-    }
-
     impl<const Q: u32> Neg for Modular<Q> {
         type Output = Modular<Q>;
 
@@ -85,9 +68,21 @@ mod modular_arithmetic {
         where
             Self: Sized,
         {
-            Modular::new(Q - self.0)
+            Modular::from([Q - self.0])
         }
     }
+    macro_rules! op_assign {
+        ($func:ident, $bound:ident, $method:ident) => {
+            impl<const Q: u32> $bound<Modular<Q>> for Modular<Q> {
+                fn $func(&mut self, other: Self) {
+                    *self = self.$method(other)
+                }
+            }
+        };
+    }
+    op_assign!(add_assign, AddAssign, add);
+    op_assign!(mul_assign, MulAssign, mul);
+    op_assign!(sub_assign, SubAssign, sub);
 
     impl<const Q: u32> PartialEq for Modular<Q> {
         fn eq(&self, other: &Self) -> bool {
@@ -101,38 +96,64 @@ mod modular_arithmetic {
         #[test]
         fn test_add() {
             const Q: u32 = 13;
-            let x = Modular::<Q>::new(5);
-            let y = Modular::<Q>::new(9);
-            let x_plus_x = Modular::<Q>::new(10);
-            let x_plus_y = Modular::<Q>::new(1);
+            let x = Modular::<Q>::from([5]);
+            let y = Modular::<Q>::from([9]);
+            let x_plus_x = Modular::<Q>::from([10]);
+            let x_plus_y = Modular::<Q>::from([1]);
             assert_eq!(x + x, x_plus_x);
             assert_eq!(x + y, x_plus_y);
         }
         #[test]
         fn test_add_zero() {
             const Q: u32 = 27;
-            let x = Modular::<Q>::new(5);
-            let y = Modular::<Q>::new(0);
+            let x = Modular::<Q>::from([5]);
+            let y = Modular::<Q>::from([0]);
             assert_eq!(x + y, x);
             assert_eq!(y + x, x);
         }
         #[test]
         fn test_sub_and_neg() {
             const Q: u32 = 31;
-            let x = Modular::<Q>::new(5);
-            let y = Modular::<Q>::new(6);
-            let z = Modular::<Q>::new(1);
-            let x_minus_y = Modular::<Q>::new(Q - 1);
+            let x = Modular::<Q>::from([5]);
+            let y = Modular::<Q>::from([6]);
+            let z = Modular::<Q>::from([1]);
+            let x_minus_y = Modular::<Q>::from([Q - 1]);
             assert_eq!(x - y, x_minus_y);
             assert_eq!(x - y, -z);
         }
         #[test]
         fn test_mul() {
             const Q: u32 = 37;
-            let x = Modular::<Q>::new(13);
-            let y = Modular::<Q>::new(5);
-            let z = Modular::<Q>::new(28);
+            let x = Modular::<Q>::from([13]);
+            let y = Modular::<Q>::from([5]);
+            let z = Modular::<Q>::from([28]);
             assert_eq!(x * y, z);
+        }
+        #[test]
+        fn test_add_assign() {
+            const Q: u32 = 37;
+            let x = Modular::<Q>::from([35]);
+            let mut y = Modular::<Q>::from([2]);
+            y += x;
+            assert_eq!(y, Modular::<Q>::from([0]));
         }
     }
 }
+
+mod matrix {
+    use super::Ring;
+    use std::cell::Cell;
+    /// An element of Mat_{N x M}(R)
+    /// Each row is a (heap-allocated) array to make matrix-vector products Mx easier to compute
+    struct Matrix<R: Ring<RANK>, const RANK: usize, const ROWS: usize, const COLS: usize>([Cell<[R ; COLS]>; ROWS]);
+    impl<R: Ring<RANK>, const RANK: usize, const ROWS: usize, const COLS: usize> Matrix<R, RANK, ROWS, COLS> {
+        fn new() {
+
+        }
+
+        fn zero() {
+        }
+    }
+
+}
+
